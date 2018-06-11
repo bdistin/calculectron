d57718fb85ffbd97dd71a70e47130b6e0b597c73
bdistin <bdistin@gmail.com>
2018-06-11 10:55:46 -0500

    lint (probably)

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@ const eshiftValues = {
 	[56]: '*',
 	[57]: '(',
 	[187]: '+'
-};
+}
 
 //Add the value for the clicked button to the calcArea text input
 function num(id) {
@@ -32,9 +32,9 @@ window.onkeydown = function (e) {
 	var keyValue = String.fromCharCode(key)
 
 	//Check for shift key alterations first
-	if (e.shiftKey) calcArea.value += eshiftValues[key] || '';
+	if (e.shiftKey) calcArea.value += eshiftValues[key] || ''
 	
-	if (key === 13) calc();
-	else if ((key >= 48 && key <=57) || key === 106) calcArea.value += keyValue;
-	else if (key===189) calcArea.value += '-';
+	if (key === 13) calc()
+	else if ((key >= 48 && key <=57) || key === 106) calcArea.value += keyValue
+	else if (key===189) calcArea.value += '-'
 }
